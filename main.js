5542ec0489f6ed51f99b30b2b44b3508d2d39817
George <59363567+GogaNotClown@users.noreply.github.com>
2020-06-13 15:26:40 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// ПЕРЕМЕННЫЕ
+var message; // а-z, A-Z, 0-9, $, _
+var myMessage; //  С помощью var объявляется переменная
+myMessage = "Текст Сообщения"; // Объявление нашей рано упомянутой переменной
+// 6 Основных типов данных
+var myNumber = 10.484; // Если указывать числа, то без кавычек , если мы укажем числа в кавычках , то это будет обычный текст
+var myString = "Привет, друг!"; // Если в ковычках ничего нет, то это считается за так называемое пустое поле
+var myBoolean = true; // false | укажем true or false в кавычках , то это будет простой текст
+var myNull = null; // Значение есть , но равняется пустоте
+var myUndefined = undefined; //  Значение отсутствует
+var myObject = { // Интересная вещь
+    name: "Имя",
+    age: 16
+};
+
+// ЧИСЛА
+//console.log(40 + myNumber);
+//console.log(40 - myNumber);
+//console.log(40 * myNumber);
+//console.log(40 / myNumber);
+//myNumber++; // -- ** //
+//console.log(myNumber);
+//console.log(Math.floor(5.9)); // методы : round , ceil (округляет в большую сторону) , floor (округляет в меньшую сторону)
+
+//var newNumber = 2.457; // + Делает округление
+//console.log(newNumber.toFixed(1)); // Количество знаков после точки
+
+// Строки
+console.log("40" + myNumber); // Теперь число 40 - строка
